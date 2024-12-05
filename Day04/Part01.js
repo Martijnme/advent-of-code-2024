a703bb491cc6a7d4862508b91a2f9d5187283c72
mengel <martijnengel2307@icloud.com>
2024-12-05 23:26:24 +0100

    add solution for day04

--- Day04/Part01.js
+++ Day04/Part01.js
@@ -1,2 +1,45 @@
 const readFile = require("../commons/readFile");
 const lines = readFile("Day04/input.txt").lines;
+
+const directions = {
+  up: [-1, 0],
+  down: [1, 0],
+  left: [0, -1],
+  right: [0, 1],
+  upLeft: [-1, -1],
+  upRight: [-1, 1],
+  downLeft: [1, -1],
+  downRight: [1, 1],
+};
+
+const countOccurrences = (lines, target) => {
+  let count = 0;
+  for (let i = 0; i < lines.length; i++) {
+    for (let j = 0; j < lines[i].length; j++) {
+      if (lines[i][j] !== target[0]) continue;
+      for (const key in directions) {
+        let [x, y] = directions[key];
+        let row = i + x;
+        let col = j + y;
+        let wordLength = 1;
+        while (
+          row >= 0 &&
+          row < lines.length &&
+          col >= 0 &&
+          col < lines[i].length &&
+          wordLength < target.length
+        ) {
+          if (lines[row][col] !== target[wordLength]) break;
+
+          wordLength++;
+          row += x;
+          col += y;
+        }
+        if (wordLength === target.length) count++;
+      }
+    }
+  }
+  return count;
+};
+
+console.log(countOccurrences(lines, "XMAS"));
--- Day04/Part02.js
+++ Day04/Part02.js
@@ -1,2 +1,53 @@
 const readFile = require("../commons/readFile");
 const lines = readFile("Day04/input.txt").lines;
+
+const directions = [
+  [-1, -1, 1, 1], // Top-left to bottom-right
+  [-1, 1, 1, -1], // Top-right to bottom-left
+];
+
+const isMASPattern = (grid, row, col, target) => {
+  let diagonalMatch = 0;
+  for (const key in directions) {
+    const [topX, topY, bottomX, bottomY] = directions[key];
+    let topRow = row + topX;
+    let topCol = col + topY;
+    let bottomRow = row + bottomX;
+    let bottomCol = col + bottomY;
+    while (
+      topRow >= 0 &&
+      topRow < grid.length &&
+      topCol >= 0 &&
+      topCol < grid[row].length &&
+      bottomRow >= 0 &&
+      bottomRow < grid.length &&
+      bottomCol >= 0 &&
+      bottomCol < grid[row].length &&
+      diagonalMatch < 2
+    ) {
+      if (
+        (grid[topRow][topCol] === target[0] &&
+          grid[bottomRow][bottomCol] === target[2]) ||
+        (grid[topRow][topCol] === target[2] &&
+          grid[bottomRow][bottomCol] === target[0])
+      ) {
+        diagonalMatch++;
+      }
+      break;
+    }
+  }
+  return diagonalMatch === 2;
+};
+
+const countCrossOccurrences = (grid, target) => {
+  let count = 0;
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] !== target[1]) continue;
+      if (isMASPattern(grid, i, j, target)) count++;
+    }
+  }
+  return count;
+};
+
+console.log(countCrossOccurrences(lines, "MAS"));
