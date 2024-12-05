cb54c8070b2618df7cf04c9ee73d6a5ba6bee28a
mengel <martijnengel2307@icloud.com>
2024-12-05 22:18:04 +0100

    add solution day03

--- Day03/Part01.js
+++ Day03/Part01.js
@@ -1,2 +1,18 @@
 const readFile = require("../commons/readFile");
-const lines = readFile("Day03/input.txt").lines;
+const data = readFile("Day03/input.txt").data;
+
+const calculateValidSum = (input) => {
+  const regex = /mul\((\d+),(\d+)\)/g;
+  let ans = 0;
+
+  const matches = input.matchAll(regex);
+
+  for (const match of matches) {
+    const [_, a, b] = match;
+    ans += a * b;
+  }
+
+  return ans;
+};
+
+console.log(calculateValidSum(data));
--- Day03/Part02.js
+++ Day03/Part02.js
@@ -1,2 +1,24 @@
 const readFile = require("../commons/readFile");
-const lines = readFile("Day03/input.txt").lines;
+const data = readFile("Day03/input.txt").data;
+
+const reduceValidParts = (input) => {
+  const [first, ...rest] = input.split("don't()");
+  return first + rest.map((entry) => entry.split("do()").slice(1)).join("");
+};
+const calculateValidSum = (input) => {
+  validPart = reduceValidParts(input);
+
+  const regex = /mul\((\d+),(\d+)\)/g;
+  let ans = 0;
+
+  const matches = validPart.matchAll(regex);
+
+  for (const match of matches) {
+    const [_, a, b] = match;
+    ans += a * b;
+  }
+
+  return ans;
+};
+
+console.log(calculateValidSum(data));
